7555466f070008861837f3d850bd40ca8e9f61ce
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-03 13:04:09 -0600

    syntax and data type exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,36 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello,");
-        System.out.print(" World!");
+
+        int myFavoriteNumber = 24;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Geronimo";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+       /* int x = 5;
+       * System.out.println(x++);
+       * System.out.println(x);
+        */
+
+        /* int x = 4;
+        * x += 5;
+        */
+
+        /*
+        * int x = 3;
+        * int y = 4;
+        * y *= x;
+        System.out.println(y); */
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        System.out.println(x);
+        System.out.println(y);
     }
 }
