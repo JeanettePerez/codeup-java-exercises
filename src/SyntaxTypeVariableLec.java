1509878c9a73bbf859193ce0c6131713eb831348
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-03 13:04:28 -0600

    lec file

---
+++ src/SyntaxTypeVariableLec.java
@@ -0,0 +1,9 @@
+public class SyntaxTypeVariableLec {
+
+    public static void main(String[] args) {
+       int myInt = -2_147_483_648;
+
+        System.out.println(myInt - 1
+        );
+    }
+}
