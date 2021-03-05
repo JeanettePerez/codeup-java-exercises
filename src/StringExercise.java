c4c18742843b748809423e04d812f3eb5a29c1cf
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-05 12:14:24 -0600

    exercise with displaying characters within a string

---
+++ src/StringExercise.java
@@ -0,0 +1,21 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String pinkFloydLyrics = "We don't need no education\n"
+                + "We don't need no thought control\n";
+        System.out.println(pinkFloydLyrics);
+
+        String stringWithQuotes = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(stringWithQuotes);
+
+        String singleBackslash = "In windows, the main drive is usually C:\\\n";
+        System.out.println(singleBackslash);
+
+        String backslashCombo = "I can do backslashes \\, \\\\,\nand the amazing triple backslash \\\\\\";
+        System.out.println(backslashCombo);
+
+
+    }
+
+}
