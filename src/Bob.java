19783744cdcf7f1ec74ba9a5303b856ef8adcda9
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-09 07:30:40 -0600

    minor changes

--- src/Bob.java
+++ src/Bob.java
@@ -5,20 +5,21 @@ public class Bob {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("hi, im bob. ask me anything");
-        String userInputQuestion = sc.nextLine();
+        System.out.println("Say something to bob..");
+        String userInput = sc.nextLine();
+
         String noResponse = "";
         String bobQuestionResponse = "sure";
         String bobExclamationResponse = "Whoa, Chill out!";
 
-        boolean endsWithExclamationPoint = (userInputQuestion.endsWith("!"));
-        boolean endsWithQuestionMark = (userInputQuestion.endsWith("?"));
+        boolean endsWithExclamationPoint = (userInput.endsWith("!"));
+        boolean endsWithQuestionMark = (userInput.endsWith("?"));
 
         if (endsWithQuestionMark)
             System.out.println(bobQuestionResponse);
         else if (endsWithExclamationPoint)
             System.out.println(bobExclamationResponse);
-        else if(userInputQuestion.equals(noResponse))
+        else if(userInput.equals(noResponse))
             System.out.println("Fine, be that way");
          else {
             System.out.println("whatever");
