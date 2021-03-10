3ec1167d1ce463564cb2308546c73d7c8331232b
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-10 12:40:33 -0600

    hiding implementation details and creating getters and setters

--- src/Oop/Dish.java
+++ src/Oop/Dish.java
@@ -2,15 +2,42 @@ package Oop;
 
 public class Dish {
 
-    public static String nameOfDish;
-    public static boolean wouldRecommend;
-    public static float costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
+    private float costInCents;
 
 
-    public void printSummary() {
-        System.out.printf("Cost: %s\nName: %s\nRecommend: %b\n", DishTools.centToDollars(), nameOfDish, wouldRecommend);
+    public void printSummary(Dish dish) {
+        System.out.printf(
+                "Cost: %s\nName: %s\nRecommend: %b\n",
+                DishTools.centToDollars(dish),
+                getNameOfDish(),
+                isWouldRecommend());
 
     }
 
 
+    public String getNameOfDish() {
+        return nameOfDish;
+    }
+
+    public void setNameOfDish(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
+
+    public boolean isWouldRecommend() {
+        return wouldRecommend;
+    }
+
+    public void setWouldRecommend(boolean wouldRecommend) {
+        this.wouldRecommend = wouldRecommend;
+    }
+
+    public float getCostInCents() {
+        return costInCents;
+    }
+
+    public void setCostInCents(float costInCents) {
+        this.costInCents = costInCents;
+    }
 }
