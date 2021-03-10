d41db24ce8f3ae800e055a9dc0aec856816cbfc0
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-10 12:43:24 -0600

    changing how data is set in the methods

--- src/Oop/DishTools.java
+++ src/Oop/DishTools.java
@@ -5,29 +5,33 @@ import java.text.NumberFormat;
 
 public class DishTools {
 
-    public static final int AVERAGE_COST_OF_DISH_IN_CENTS = 1_300;
+    private static final int AVERAGE_COST_OF_DISH_IN_CENTS = 1_300;
 
-    public static void shoutDishName() {
-        System.out.println(Dish.nameOfDish.toUpperCase());
+
+    public static void shoutDishName(Dish dish) {
+        System.out.println("name: " + dish.getNameOfDish().toUpperCase());
     }
 
-    public static void analyzeDishCost() {
-
-        if(Dish.costInCents > AVERAGE_COST_OF_DISH_IN_CENTS)
-            System.out.println("More expensive than average");
-        else if(Dish.costInCents < AVERAGE_COST_OF_DISH_IN_CENTS)
-            System.out.println("less expensive than average");
+    public static void analyzeDishCost(Dish dish) {
+        if(dish.getCostInCents() > AVERAGE_COST_OF_DISH_IN_CENTS) {
+            System.out.println(dish.getNameOfDish() + ": is higher than the average");
+        }
+        else if(dish.getCostInCents() < AVERAGE_COST_OF_DISH_IN_CENTS) {
+            System.out.println(dish.getNameOfDish() + ": is less than the average");
+        }
         else
-            System.out.println(centToDollars()
-                    + " is the average cost of of a meal in the US");
+            System.out.println("the cost of " + dish.getNameOfDish() + " is the average for a meal in the US");
     }
 
-    public static void flipRecommendation() {
-        System.out.println(!Dish.wouldRecommend);
+    public static void flipRecommendation(Dish dish) {
+        System.out.println("Recommend: " + !dish.isWouldRecommend());
     }
 
 
-    public static String centToDollars() {
-        return NumberFormat.getCurrencyInstance().format(Dish.costInCents / 100.);
+    public static String centToDollars(Dish dish) {
+
+        return NumberFormat
+                .getCurrencyInstance()
+                .format(dish.getCostInCents() / 100.);
     }
 }
