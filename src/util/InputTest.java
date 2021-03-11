2958dc9cc38838b7f2bb4f838aef059fa5694e03
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-11 10:13:32 -0600

    testing out my input class

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -5,9 +5,22 @@ public class InputTest {
 
     public static void main(String[] args) {
         Input input1 = new Input();
-//        input1.getInt(1,10);
-//        input1.getDouble();
-        input1.getDouble(1,10);
+
+        String stringInput = input1.getString("enter a sentence: ");
+        System.out.println(stringInput);
+
+//        double intNumberRange = input1.getInt(1,10);
+//        System.out.println(intNumberRange);
+
+
+//        double numberRange = input1.getDouble(1,10);
+//        System.out.println(numberRange);
+
+
+//        boolean willContinue = input1.yesNo("will like to continue?");
+//        System.out.println(willContinue);
+
+
 
     }
 
