b6c28c97d6a1f82cf62dd35c3e916ddc1f738319
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-13 10:09:48 -0600

    walkthrough notes

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -3,11 +3,16 @@ import java.util.Random;
 public class ServerNameGenerator {
     public static void main(String[] args) {
         System.out.println(nameGenerator());
+
+        // Walk through example
+        String[] adjectives = { "artistic", "charming", "graceful", "rough", "thankful", "helpful", "bitter", "petty", "fancy", "responsive" };
+        String[] nouns = { "spoon", "dog", "earth", "toaster", "texas", "school", "ocean", "woman", "coffee", "fruit" };
+        System.out.println(getWord(adjectives) + "-" + getWord(nouns));
     }
 
     public static String nameGenerator(){
         String[] adjectives = { "artistic", "charming", "graceful", "rough", "thankful", "helpful", "bitter", "petty", "fancy", "responsive" };
-        String[] nouns = { "spoon", "dog", "earth", "toaster", "texas", "school", "ocean", "woman", "coffee", "fruit"};
+        String[] nouns = { "spoon", "dog", "earth", "toaster", "texas", "school", "ocean", "woman", "coffee", "fruit" };
         Random random = new Random();
         int randomAdj = random.nextInt(adjectives.length);
         int randomNoun = random.nextInt(nouns.length);
@@ -17,5 +22,11 @@ public class ServerNameGenerator {
         return message + randomName;
     }
 
+    // walk through example
+    public static String getWord(String[] choices) {
+        Random random = new Random();
+        int randomWord = random.nextInt(choices.length);
+        return choices[randomWord];
+    }
 
 }
