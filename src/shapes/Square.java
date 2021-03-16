891bba85b0a555d4f789590eb621450485d8a652
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-16 17:14:29 -0500

    inheritance from Quad.

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,9 +1,16 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
 
     public Square(double side) {
         super(side, side);
+    }
+
+
+    @Override
+    public double getPerimeter() {
+        System.out.println("getting perimeter from Square class");
+        return 4 * length;
     }
 
     @Override
@@ -13,8 +20,29 @@ public class Square extends Rectangle{
     }
 
     @Override
-    public double getPerimeter() {
-        System.out.println("getting perimeter from Square class");
-        return 4 * length;
+    public void setLength(double length) {
+
     }
+
+    @Override
+    public void setWidth(double width) {
+
+    }
+
+    // ========= inheritance Exercise
+//    public Square(double side) {
+//        super(side, side);
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        System.out.println("getting area from the Square class");
+//        return Math.pow(length, 2);
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        System.out.println("getting perimeter from Square class");
+//        return 4 * length;
+//    }
 }
