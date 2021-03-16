f7aeb5f418ec8cfddff6121fd76048c302c420df
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-16 17:15:20 -0500

    extending quad.

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,44 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
-
+public class Rectangle extends Quadrilateral {
     public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
+        super(length, width);
     }
 
-    public double getArea(){
-        return length * width;
+    @Override
+    public void setLength(double length) {
+
     }
 
-    public double getPerimeter(){
+    @Override
+    public void setWidth(double width) {
+
+    }
+
+
+    @Override
+    public double getPerimeter() {
         return 2 * length + 2 * width;
     }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+    //========= inheritance Exercise
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getArea(){
+//        return length * width;
+//    }
+//
+//    public double getPerimeter(){
+//        return 2 * length + 2 * width;
+//    }
 }
