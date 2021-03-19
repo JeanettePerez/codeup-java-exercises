686f67f06c3d29b68f0f4cffd5d12c5a3089775f
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-19 12:29:09 -0500

    adding walkthrough notes

---
+++ src/miniExercises/ArrayListExercise.java
@@ -0,0 +1,33 @@
+package miniExercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExercise {
+    public static void main(String[] args) {
+        // === adding dog names to the list ===
+//        List<String> dogNames = new ArrayList<>();
+//        dogNames.add("Grim");
+//        dogNames.add("Thor");
+//        dogNames.add("Indie");
+//        dogNames.add("Mellow");
+//
+//        System.out.println(dogNames); // [Grim, Thor, Indie, Mellow]
+
+        // === removing dog names from the list ===
+        List<String> dogNames = new ArrayList<>();
+        dogNames.add("Grim");
+        dogNames.add("Thor");
+        dogNames.add("Indie");
+        dogNames.add("Mellow");
+
+//      === removing by index and then commenting it out before removing by object ===
+//        dogNames.remove(2); // removes indie that is at index 2
+//        System.out.println(dogNames);
+//         dogNames.remove("Mellow");
+//         System.out.println(dogNames);
+
+    }
+
+
+}
