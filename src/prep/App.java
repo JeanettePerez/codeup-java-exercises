fe028a09c5c5000d296e0ff9946eec9d990fc9b6
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-28 16:26:01 -0500

    Class holding the main method to test program practice

--- src/prep/App.java
+++ src/prep/App.java
@@ -1,22 +1,32 @@
 package prep;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class App {
 
     public static void main(String[] args) {
-        int square = Arithmetic.square(10);
-        System.out.println(square);
+        // ========= Activity 1 =========
+//        int square = Arithmetic.square(10);
+//        System.out.println(square);
+//
+//        int sum = Arithmetic.sum(2,5);
+//        System.out.println(sum);
+//
+//        int difference = Arithmetic.difference(10,5);
+//        System.out.println(difference);
+//
+//        int product = Arithmetic.product(2,2);
+//        System.out.println(product);
+//
+//        double average = Arithmetic.average(new double[]{1, 2, 3, 4});
+//        System.out.println(average);
+       // ========= Activity 2 =========
+        var foods = new ArrayList<>();
+       foods.add(new Pizza())
 
-        int sum = Arithmetic.sum(2,5);
-        System.out.println(sum);
 
-        int difference = Arithmetic.difference(10,5);
-        System.out.println(difference);
 
-        int product = Arithmetic.product(2,2);
-        System.out.println(product);
-
-        double average = Arithmetic.average(new double[]{1, 2, 3, 4});
-        System.out.println(average);
     }
 
 }
