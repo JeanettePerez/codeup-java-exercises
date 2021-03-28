d810221f9a9f5a92b33577c519c23e352c030b1f
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-28 16:22:39 -0500

    interface implemented in Food

--- src/prep/Consumable.java
+++ src/prep/Consumable.java
@@ -1,4 +1,10 @@
 package prep;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public interface Consumable {
+    void consume();
+    List<String> getFullDescription();
 }
